e2dd14a185c7b0db025d61a866795972b21cf04a
seungjo <f1v3@kakao.com>
2024-12-16 16:47:01 +0900

    first commit

---
+++ src/main/java/com/f1v3/email/EmailApplication.java
@@ -0,0 +1,13 @@
+package com.f1v3.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f1v3/email/EmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1v3.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
